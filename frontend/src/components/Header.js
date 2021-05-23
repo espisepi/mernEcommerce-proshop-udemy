38e70fcb14c132391a3cda88d26d4eb5da4704c5
espisepi <sepinaco@gmail.com>
2021-05-23 20:30:58 +0200

    8 - implementing React Router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 // https://react-bootstrap.github.io/components/navbar/
+/* <Navbar.Brand href="/">ProShop</Navbar.Brand> when not <LinkContainer>*/ 
 const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>ProShop</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                        <Nav.Link href="/cart">
-                            <li className="fas fa-shopping-cart"></li>
-                            Cart
-                        </Nav.Link>
-                        <Nav.Link href="/login">
-                            <li className="fas fa-user"></li>
-                            Link
-                        </Nav.Link>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link>
+                                    <li className="fas fa-shopping-cart"></li>
+                                    Cart
+                                </Nav.Link>
+                            </LinkContainer>
+
+                            <LinkContainer to='/login'>
+                                <Nav.Link>
+                                    <li className="fas fa-user"></li>
+                                    Sign in
+                                </Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
