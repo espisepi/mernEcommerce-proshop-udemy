5ff9cdb1fbe95d24127bfb66289a6331500b83d7
espisepi <sepinaco@gmail.com>
2021-06-06 19:06:13 +0200

    37. User authentication endpoint

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -14,20 +15,14 @@ connectDB();
 
 const app = express();
 
+app.use(express.json()) // Para poder recibir json en el body
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 });
 
 app.use('/api/products', productRoutes);
-/** This code is old and now is in productRoutes */
-// import products from './data/products';
-// app.get('/api/products', (req, res) => {
-//     res.json(products);
-// });
-// app.get('/api/products/:id', (req, res) => {
-//     const product = products.find( p => p._id === req.params.id);
-//     res.json(product);
-// });
+app.use('/api/users', userRoutes);
 
 /* Middleware, this code always in final */
 app.use(notFound)
@@ -36,3 +31,18 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000;  // Thanks to dotenv package
 
 app.listen(PORT, console.log(`Server running in ${ process.env.NODE_ENV } on port ${ PORT }`));
+
+
+
+/** This code is old and now is in productRoutes 
+
+    import products from './data/products';
+    app.get('/api/products', (req, res) => {
+        res.json(products);
+    });
+    app.get('/api/products/:id', (req, res) => {
+        const product = products.find( p => p._id === req.params.id);
+        res.json(product);
+    });
+
+*/
